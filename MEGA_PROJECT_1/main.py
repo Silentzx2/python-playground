901579a65d3bba2d8bfbd87b480ae77956d831f2
Silentzx2 <davidsindur106@gmail.com>
2026-05-24 22:06:21 +0530

    New mega project & chapter-3 program-3 update

---
+++ MEGA_PROJECT_1/main.py
@@ -0,0 +1,60 @@
+# Project name - JARVIS
+
+
+# Modules
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+os.environ["ALSA_LOG_LEVEL"] = "0"
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musiclib
+
+
+# Initialize the recognizer
+r = sr.Recognizer()
+engine = pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def process_command(c):
+        if "youtube" in c.lower():
+            webbrowser.open("https://youtube.com")
+            speak("Opening YouTube boss")
+
+        elif "google" in c.lower():
+            webbrowser.open("https://google.com")
+            speak("Opening Google boss")
+
+        elif "github" in c.lower():
+            webbrowser.open("https://github.com")
+            speak("Opening GitHub boss")
+        elif c.lower().startswith("play"):
+             song = " ".join(c.lower().split(" ")[1:])
+             link = musiclib.music[song]
+             webbrowser.open(link)
+        elif "news" in c.lower():
+             webbrowser.open("https://www.ndtv.com/")
+             speak("opening the latest news, boss")
+             
+ 
+if __name__ == "__main__":
+    r = sr.Recognizer()
+    speak("jarvis Initializing")
+while True:
+    try:
+    # listen for the wake word "Jarvis"
+        with sr.Microphone() as source:
+               print("listening...")
+               audio = r.listen(source, timeout=5, phrase_time_limit=4)
+        word = r.recognize_google(audio)
+        if (word.lower() == "jarvis"):
+            speak("Yes boss")
+            # listen for c
+            with sr.Microphone() as source:
+                audio = r.listen(source, timeout=5, phrase_time_limit=4)
+                c = r.recognize_google(audio)
+                process_command(c)
+    except Exception as e:
+        print("Error:", e)
---
+++ MEGA_PROJECT_1/musiclib.py
@@ -0,0 +1,12 @@
+music = {
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc",
+    "shape of you": "https://www.youtube.com/watch?v=JGwWNGJdvx8",
+    "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "let me love you": "https://www.youtube.com/watch?v=euCqAq6BRa4",
+    "perfect": "https://www.youtube.com/watch?v=2Vv-BfVoq4g",
+    "attention": "https://www.youtube.com/watch?v=nfs8NYg7yQM",
+    "unstoppable": "https://www.youtube.com/watch?v=cxjvTXo9WWM",
+    "heat waves": "https://www.youtube.com/watch?v=mRD0-GxqHVo",
+    "night changes": "https://www.youtube.com/watch?v=syFZfO_wfMQ",
+    "hymn for the weekend": "https://www.youtube.com/watch?v=YykjpeuMNEk"
+}
